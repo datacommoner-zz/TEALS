1ebc0d3dfb9cf678d739cfd1705d699d6bcb09c9
“datacommoner” <datacommoner@outlook.com>
2016-05-24 16:17:45 -0700

    first commit

---
+++ StreamingTweep.py
@@ -0,0 +1,33 @@
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import time
+
+CONSUMER_KEY = '<Put API key Here>'
+CONSUMER_SECRET ='<Put API secret Here>'
+OAUTH_TOKEN = '<Put Access token Here>'
+OAUTH_TOKEN_SECRET = '<Put Access token secret Here>'
+
+
+class listener(StreamListener):
+
+	def on_data(self, data):
+		try:
+			print data
+			saveFile = open('tweets.txt', 'a')
+			saveFile.write(data)
+			saveFile.write('\n')
+			saveFile.close()
+			return True
+		except BaseException, e:
+			print 'failed ondata,',str(e)
+			time.sleep(5)
+
+	def on_error(self, status):
+		print status
+
+auth = OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+twitterStream = Stream(auth, listener())
+
+twitterStream.filter(track=["@Wimbledon", "@LiveatWimbledon", "#Wimbledon", "@ESPNTennis", "Tennis"])
